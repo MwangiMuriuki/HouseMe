da22e40aca23641632d72df9e774720b1cb72f96
Mwangi Muriuki <muriukimwangi.mm@gmail.com>
2019-02-08 17:46:23 +0300

    Commit 4. EOD. Thank You Jesus!!!

--- app/src/main/java/com/example/houseme/Login.java
+++ app/src/main/java/com/example/houseme/Login.java
@@ -1,13 +1,47 @@
 package com.example.houseme;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 public class Login extends Activity {
-
+    Button login;
+    TextView register, resetPwd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        login = findViewById(R.id.buttonLogin);
+        register = findViewById(R.id.textViewRegister);
+        resetPwd = findViewById(R.id.resetPassword);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplication(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent reg = new Intent(getApplication(), Register.class);
+                startActivity(reg);
+            }
+        });
+
+        resetPwd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent reset = new Intent(getApplication(), ResetPassword.class);
+                startActivity(reset);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/houseme/ResetPassword.java
@@ -0,0 +1,13 @@
+package com.example.houseme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ResetPassword extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_password);
+    }
+}
